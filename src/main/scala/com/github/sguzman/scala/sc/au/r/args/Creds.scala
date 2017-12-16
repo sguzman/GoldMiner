1aa91917f5a99dbc588361e20ac839e30a499126
Salvador Guzman <guzmansalv@gmail.com>
2017-12-16 07:53:36 -0800

    set default values on all args

--- src/main/scala/com/github/sguzman/scala/sc/au/r/args/Creds.scala
+++ src/main/scala/com/github/sguzman/scala/sc/au/r/args/Creds.scala
@@ -1,8 +1,8 @@
 package com.github.sguzman.scala.sc.au.r.args
 
 case class Creds(
-                perm: String,
-                pass: String,
+                perm: String = "",
+                pass: String = "",
                 old: Boolean = false,
                 verbose: Boolean = false,
                 dev: Boolean = false,
