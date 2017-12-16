fb61a4a780ddd2d77227409ca85e5213ce1c7b48
Salvador Guzman <guzmansalv@gmail.com>
2017-12-16 07:08:58 -0800

    simple get of login page

--- src/main/scala/com/github/sguzman/scala/sc/au/r/Main.scala
+++ src/main/scala/com/github/sguzman/scala/sc/au/r/Main.scala
@@ -1,5 +1,12 @@
 package com.github.sguzman.scala.sc.au.r
 
+import scalaj.http.Http
+
 object Main {
-  def main(args: Array[String]): Unit = println("Hello, world!")
+  def main(args: Array[String]): Unit = {
+    val url = "https://my.sa.ucsb.edu/gold/Login.aspx"
+    val request = Http(url)
+    val response = request.asString
+    println(response.body)
+  }
 }
