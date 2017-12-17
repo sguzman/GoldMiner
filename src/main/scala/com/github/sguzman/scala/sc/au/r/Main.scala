cb12c1505c64e0ca0743087f561432b2330e9f73
Salvador Guzman <guzmansalv@gmail.com>
2017-12-16 20:38:31 -0800

    simple login for old account

--- src/main/scala/com/github/sguzman/scala/sc/au/r/Main.scala
+++ src/main/scala/com/github/sguzman/scala/sc/au/r/Main.scala
@@ -1,5 +1,8 @@
 package com.github.sguzman.scala.sc.au.r
 
+import java.net.URLEncoder.encode
+import java.nio.charset.StandardCharsets.UTF_8
+
 import com.github.sguzman.scala.sc.au.r.args.{Creds, CredsConfig}
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
@@ -10,16 +13,31 @@ import scalaj.http.Http
 
 object Main {
   def main(args: Array[String]): Unit = util.Try({
-    val config = new CredsConfig().parse(args, Creds())
-    if (config.isEmpty) throw new Exception("Invalid cmd args")
+    val configOpt = new CredsConfig().parse(args, Creds())
+    if (configOpt.isEmpty) throw new Exception("Invalid cmd args")
+    val config = configOpt.get
 
-    val url = "https://my.sa.ucsb.edu/gold/Login.aspx"
-    val request = Http(url)
-    val response = request.asString
+    val response = Http("https://my.sa.ucsb.edu/gold/Login.aspx").asString
+
     val doc = JsoupBrowser().parseString(response.body)
     val hidden = doc >> elementList("""input[type="hidden"]""")
-    val hiddenPairs = hidden map (_.attr("name")) zip (hidden map (_.attr("value")))
-    println(hiddenPairs)
+    val hiddenPairs = hidden map (_.attr("name")) zip (hidden map (_.attr("value")) map (encode(_, UTF_8.toString)))
+
+    val logins = (if (config.old) List(
+      "ctl00%24pageContent%24PermPinLogin%24userNameText",
+      "ctl00%24pageContent%24PermPinLogin%24passwordText",
+      "ctl00%24pageContent%24PermPinLogin%24loginButton"
+    ) else List(
+      "ctl00%24pageContent%24userNameText",
+      "ctl00%24pageContent%24passwordText",
+      "ctl00%24pageContent%24loginButton"
+    )) zip List(config.perm, config.pass, "Login")
+
+    val bodyPairs = hiddenPairs ++ logins
+    val bodyStr = bodyPairs.map(t => s"${t._1}=${t._2}").mkString("&")
+
+    val postResponse = Http("https://my.sa.ucsb.edu/gold/Login.aspx").postData(bodyStr).asString
+    println(postResponse)
   }) match {
     case Success(_) => println("done")
     case Failure(e) => Console.err.println(e)
