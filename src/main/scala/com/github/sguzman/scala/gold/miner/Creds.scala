df3d8784199b0e2563bec068a67d031d13f586e9
Salvador Guzman <guzmansalv@gmail.com>
2018-01-08 18:50:13 -0800

    simple login working

--- src/main/scala/com/github/sguzman/scala/gold/miner/Creds.scala
+++ src/main/scala/com/github/sguzman/scala/gold/miner/Creds.scala
@@ -6,4 +6,14 @@ case class Creds(
   old: Boolean = false,
   dev: Boolean = false,
   help: Boolean = false
-)
+) {
+  def hid = (if (this.old) List(
+    "ctl00%24pageContent%24PermPinLogin%24userNameText",
+    "ctl00%24pageContent%24PermPinLogin%24passwordText",
+    "ctl00%24pageContent%24PermPinLogin%24loginButton"
+  ) else List(
+    "ctl00%24pageContent%24userNameText",
+    "ctl00%24pageContent%24passwordText",
+    "ctl00%24pageContent%24loginButton"
+  )) zip List(this.perm, this.pass, "Login")
+}
--- src/main/scala/com/github/sguzman/scala/gold/miner/Main.scala
+++ src/main/scala/com/github/sguzman/scala/gold/miner/Main.scala
@@ -1,20 +1,64 @@
 package com.github.sguzman.scala.gold.miner
 
-import scala.language.implicitConversions
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+
+import net.ruippeixotog.scalascraper.browser.{Browser, JsoupBrowser}
+import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
+import net.ruippeixotog.scalascraper.dsl.DSL._
+import net.ruippeixotog.scalascraper.model.Document
+import net.ruippeixotog.scalascraper.model.Element
+
+import scala.language.{implicitConversions, postfixOps}
 import org.scalatest.Matchers._
+
+import scalaj.http.{Http, HttpRequest, HttpResponse}
 
 object Main {
   final case class ParseCreds(args: Array[String]) {
     def parse: Creds = {
       val creds = CredsConfig().parse(args, Creds())
       creds should not be None
+
       creds.get
+    }
+
+    def login: HttpRequest = {
+      "https://my.sa.ucsb.edu/gold/Login.aspx".login(args.parse)
     }
   }
   implicit def argsToParse(args: Array[String]): ParseCreds = ParseCreds(args)
 
+  final case class GeneralStr(str: String) {
+    def get: HttpResponse[String] = {
+      val response = Http(str).asString
+      response.is2xx should be (true)
+      response.isSuccess should be (true)
+
+      response
+    }
+    def log(url: String): HttpRequest = Http(url).postData(str)
+
+    def doc: Browser#DocumentType = JsoupBrowser().parseString(str)
+
+    def elems(doc: Document): List[Element] = doc >> elementList(str)
+
+    def enc: String = URLEncoder.encode(str, StandardCharsets.UTF_8.toString)
+
+    def attr(ele: Element): String = ele.attr(str)
+
+    def hidden: List[(String, String)] = {
+      val hidden = """input[type="hidden"]""".elems("https://my.sa.ucsb.edu/gold/Login.aspx".get.body.doc)
+      hidden map "name".attr zip (hidden map "value".attr map (_.enc))
+    }
+
+    def login(cred: Creds): HttpRequest = (str.hidden ++ cred.hid).map(t => s"${t._1}=${t._2}").mkString("&").log(str)
+  }
+
+  implicit def strToGeneral(url: String): GeneralStr = GeneralStr(url)
+
   def main(args: Array[String]): Unit = {
-    val creds = args.parse
-    println(creds)
+    val login = args.login.asString
+    println(login)
   }
 }
