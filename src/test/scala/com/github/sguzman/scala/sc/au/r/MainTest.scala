ff4d8196fc33001ac36cc8895133bf683f3896e7
Salvador Guzman <guzmansalv@gmail.com>
2018-01-08 16:46:25 -0800

    use system.var to set user/pass

--- src/test/scala/com/github/sguzman/scala/sc/au/r/MainTest.scala
+++ src/test/scala/com/github/sguzman/scala/sc/au/r/MainTest.scala
@@ -1,9 +1,16 @@
 package com.github.sguzman.scala.sc.au.r
 
 import org.scalatest.FlatSpec
+import org.scalatest.Matchers._
 
 class MainTest extends FlatSpec {
   "App" should "run without errors" in {
-    Main.main(Array())
+    val user = System.getenv("USERNAME")
+    user should not be null
+
+    val pass = System.getenv("PASSWORD")
+    pass should not be null
+
+    Main.main(Array("--perm", user,"--pass", pass, "--old", "--dev"))
   }
 }
