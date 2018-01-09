1e77a2445257c308d7e64edf85f1f96e421987d1
Salvador Guzman <guzmansalv@gmail.com>
2018-01-08 22:09:38 -0800

    working solution that retrieves quarters and departments

--- src/main/scala/com/github/sguzman/scala/gold/miner/Main.scala
+++ src/main/scala/com/github/sguzman/scala/gold/miner/Main.scala
@@ -15,6 +15,7 @@ import org.scalatest.Matchers._
 import scalaj.http.{Http, HttpRequest, HttpResponse}
 
 object Main {
+  implicit def argsToParse(args: Array[String]): ParseCreds = ParseCreds(args)
   final case class ParseCreds(args: Array[String]) {
     def parse: Creds = {
       val creds = CredsConfig().parse(args, Creds())
@@ -23,12 +24,30 @@ object Main {
       creds.get
     }
 
-    def login: HttpRequest = {
-      "https://my.sa.ucsb.edu/gold/Login.aspx".login(args.parse)
-    }
-  }
-  implicit def argsToParse(args: Array[String]): ParseCreds = ParseCreds(args)
+    def req: HttpRequest = "https://my.sa.ucsb.edu/gold/Login.aspx" login args.parse
 
+    def login: HttpResponse[String] = this.req.asString
+
+    def courses: HttpResponse[String] = this.req.asString.courses
+
+    def quarters: List[String] = this.login.courses.quarters
+    def quarters(state: HttpResponse[String]): List[String] = state.quarters
+
+    def departments: List[String] = this.login.courses.departments
+    def departments(state: HttpResponse[String]): List[String] = state.departments
+
+    private def argumentsDev(state: HttpResponse[String], quarter: String): List[(String, String)] =
+      this.departments(state).map(b => (quarter, b))
+
+    private def argumentsRaw(state: HttpResponse[String]): List[(String, String)] = if (args.parse.dev)
+      this.argumentsDev(state, this.quarters(state).head)
+    else
+      this.quarters(state) flatMap (a => this.departments(state).map(b => (a,b)))
+
+    def arguments: List[(String, String)] = this.argumentsRaw(this.courses)
+  }
+
+  implicit def strToGeneral(url: String): GeneralStr = GeneralStr(url)
   final case class GeneralStr(str: String) {
     def get: HttpResponse[String] = {
       val response = Http(str).asString
@@ -37,6 +56,7 @@ object Main {
 
       response
     }
+
     def log(url: String): HttpRequest = Http(url).postData(str)
 
     def doc: Browser#DocumentType = JsoupBrowser().parseString(str)
@@ -47,18 +67,28 @@ object Main {
 
     def attr(ele: Element): String = ele.attr(str)
 
-    def hidden: List[(String, String)] = {
-      val hidden = """input[type="hidden"]""".elems("https://my.sa.ucsb.edu/gold/Login.aspx".get.body.doc)
-      hidden map "name".attr zip (hidden map "value".attr map (_.enc))
-    }
+    private def hiddenRaw(es: List[Element]) = es map "name".attr zip (es map "value".attr map (_.enc))
+    def hidden: List[(String, String)] =
+      this.hiddenRaw("""input[type="hidden"]""".elems("https://my.sa.ucsb.edu/gold/Login.aspx".get.body.doc))
 
     def login(cred: Creds): HttpRequest = (str.hidden ++ cred.hid).map(t => s"${t._1}=${t._2}").mkString("&").log(str)
   }
 
-  implicit def strToGeneral(url: String): GeneralStr = GeneralStr(url)
+  implicit def HttpResponse(http: HttpResponse[String]): HttpResponseAddendum = HttpResponseAddendum(http)
+  final case class HttpResponseAddendum(http: HttpResponse[String]) {
+    def nextReq(url: String): HttpRequest = Http(url).header("Cookie", this.cookie)
+    def next(url: String): HttpResponse[String] = this.nextReq(url).asString
+
+    def cookie: String = http.cookies.mkString("; ")
+
+    def courses: HttpResponse[String] = this.next("https://my.sa.ucsb.edu/gold/BasicFindCourses.aspx")
+    def quarters: List[String] = "#pageContent_quarterDropDown > option" elems http.body.doc map "value".attr
+
+    private def departmentsRaw = "#pageContent_subjectAreaDropDown > option" elems http.body.doc map "value".attr
+    def departments: List[String] = this.departmentsRaw.tail
+  }
 
   def main(args: Array[String]): Unit = {
-    val login = args.login.asString
-    println(login)
+    println(args.arguments)
   }
 }
