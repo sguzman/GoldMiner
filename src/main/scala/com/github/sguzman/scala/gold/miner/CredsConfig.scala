ae4bf4b168e1ff6a4b3f88a95cd423dc2e2e69e4
Salvador Guzman <guzmansalv@gmail.com>
2018-01-08 17:57:41 -0800

    add creds config class

---
+++ src/main/scala/com/github/sguzman/scala/gold/miner/CredsConfig.scala
@@ -0,0 +1,34 @@
+package com.github.sguzman.scala.gold.miner
+
+class CredsConfig extends scopt.OptionParser[Creds]("GoldMiner") {
+  head("GoldMiner", "1.0")
+
+  opt[String]('u', "perm")
+    .text("Perm Number")
+    .required()
+    .valueName("<perm>")
+    .minOccurs(1)
+    .maxOccurs(1)
+    .action((x, c) => c.copy(perm = x))
+
+  opt[String]('p', "pass")
+    .text("Password")
+    .required()
+    .valueName("<pass>")
+    .minOccurs(1)
+    .maxOccurs(1)
+    .action((x, c) => c.copy(pass = x))
+
+  opt[Unit]('o', "old")
+    .text("Is an old account?")
+    .optional()
+    .action((x, c) => c.copy(old = true))
+
+  opt[Unit]('d', "dev")
+    .text("Is this development mode?")
+    .optional()
+    .action((x, c) => c.copy(dev = true))
+
+  help("help")
+    .text("this menu")
+}
