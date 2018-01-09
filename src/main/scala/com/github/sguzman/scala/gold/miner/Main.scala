ec717389bdd0dbea1aacc3cd8b72bd3106d74a5c
Salvador Guzman <guzmansalv@gmail.com>
2018-01-08 17:41:10 -0800

    started from scratch

---
+++ src/main/scala/com/github/sguzman/scala/gold/miner/Main.scala
@@ -0,0 +1,7 @@
+package com.github.sguzman.scala.gold.miner
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+}
--- src/main/scala/com/github/sguzman/scala/sc/au/r/args/Creds.scala
+++
@@ -1,9 +0,0 @@
-package com.github.sguzman.scala.sc.au.r.args
-
-case class Creds(
-                perm: String = "",
-                pass: String = "",
-                old: Boolean = false,
-                dev: Boolean = false,
-                help: Boolean = false
-                )
