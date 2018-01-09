4ee69a235e8e5523f9db150d2e8b1664afe10258
Salvador Guzman <guzmansalv@gmail.com>
2018-01-08 16:27:11 -0800

    super simple test case should just call main

--- src/test/scala/com/github/sguzman/scala/sc/au/r/MainTest.scala
+++ src/test/scala/com/github/sguzman/scala/sc/au/r/MainTest.scala
@@ -1,5 +1,9 @@
 package com.github.sguzman.scala.sc.au.r
 
-class MainTest {
+import org.scalatest.FlatSpec
 
+class MainTest extends FlatSpec {
+  "App" should "run without errors" in {
+    Main.main(Array())
+  }
 }
